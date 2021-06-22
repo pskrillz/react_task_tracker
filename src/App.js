a15ae16f31f61943419ea48712958e9debae4912
pi <payam.farah@live.com>
2021-06-22 15:54:34 -0700

    moving dumby data to app root for global scoping between components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
 import Header from './Components/Header';
 import Button from './Components/Button';
-import Tasks from './Components/Tasks'
+import Tasks from './Components/Tasks';
+import { useState } from "react"
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1, 
+        text: "Doctor Appointment", 
+        day: "March 6th, 2020 at 12:00pm",
+        reminder: true
+    },
+    {
+        id: 2, 
+        text: "Meeting at School",
+        day: 'February 8th, 2020 at 11:00am',
+        reminder: false
+    },
+    {   
+        id: 3, 
+        text: "Big Exam",
+        day: "April 11th, 2020 at 06:00pm",
+        reminder: true
+    }
+])
+
   const onClick = () =>{
     console.log("root click")
   }
@@ -13,7 +35,7 @@ function App() {
       <Header></Header>
       {/* No need for this test button
        <Button color="green" text="root click" onClick={onClick}></Button> */}
-      <Tasks/>
+      <Tasks taskList={tasks}/>
       
     </div>
   );
--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,29 +1,15 @@
 
+
+
 // this provides the content of the tasks
-const dumbyTaskList = [
-    {
-        id: 1, 
-        text: "Doctor Appointment", 
-        day: "March 6th, 2020 at 12:00pm",
-        reminder: true
-    },
-    {
-        id: 2, 
-        text: "Meeting at School",
-        day: 'February 8th, 2020 at 11:00am',
-        reminder: false
-    },
-    {   
-        id: 3, 
-        text: "Big Exam",
-        day: "April 11th, 2020 at 06:00pm",
-        reminder: true
-    }
-]
-function Tasks() {
+const Tasks = ({taskList}) => {
+   
+
+
+
     return (
         <div>
-            {dumbyTaskList.map( (task) => 
+            {taskList.map( (task) => 
             (<h3 key={task.id}>{task.text}</h3>
             ))}
         </div>
