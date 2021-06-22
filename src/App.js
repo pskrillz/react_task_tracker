0d1006533fee8e06f8771c940d9a7f127e825716
pi <payam.farah@live.com>
2021-06-21 17:31:01 -0700

    added first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Task Tracker</h1>
+      <Header/>
     </div>
   );
 }
