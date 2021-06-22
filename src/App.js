4ebf2f27dd9e16f60cb7d8d97bf5532beaebb950
pi <payam.farah@live.com>
2021-06-21 23:20:18 -0700

    more experimenting with passing data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './Components/Header';
+import Button from './Components/Button';
 
 function App() {
+  const onClick = () =>{
+    console.log("root click")
+  }
+
   return (
     <div className="container">
       <Header></Header>
+      <Button color="green" text="root click" onClick={onClick}></Button>
       
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
     return (
         <header class="header">
         <h1>Task Tracker</h1>
-       <Button color="blue" text="Click"
+       <Button color="blue" text="header click"
        onClick={onClick}></Button>
 
 
