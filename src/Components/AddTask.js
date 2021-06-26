cbfef80e7639ac07c4a463fa61eab325ecad456c
pi <payam.farah@live.com>
2021-06-25 17:41:04 -0700

    added state hooks for the add task component

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -1,21 +1,37 @@
-
+import { useState } from "react"
 
 const AddTask = () => {
+    const [text, setText] = useState('');
+    const [day, setDay] = useState('');
+    const [reminder, setReminder] = useState(false);
+
+
     return (
         <form className="add-form">
             <div className="form-control">
                 <label> Task</label>
-                <input type="text" placeholder="Add Task"  />
+                <input type="text" 
+                placeholder="Add Task"
+                value={text} 
+                onChange={(e) => setText(e.target.value)}
+                />
             </div>
             <div className="form-control">
                 <label>Day and Time</label>
-                <input type="text" placeholder="Add Day"  />
+                <input type="text" 
+                placeholder="Add Day"
+                value={day}
+                onChange={(e) => setDay(e.target.value)}                
+                />
             </div>
-            <div className="form-control">
+            <div className="form-control form-control-check">
                 <label>Set Reminder</label>
-                <input type="checkbox"   />
+                <input type="checkbox"   
+                value={reminder}
+                onChange={(e) => setReminder(e.currentTarget.checked)}
+                />
             </div>
-         <input type="submit" value="Save Task"/>
+         <input type="submit" className="btn btn-block" value="Save Task"/>
             
         </form>
     )
